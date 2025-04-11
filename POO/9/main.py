1b9e93defef1f6b4b738295440958560926127a4
Fabri-404 <135560802+Fabri-404@users.noreply.github.com>
2025-04-11 01:32:24 -0400

    SE AGREGO SU ENUNCIADO #9

--- POO/9/main.py
+++ POO/9/main.py
@@ -1,4 +1,11 @@
+"""
+---------------ENUNCIADO------------------
 
+9. Realiza la abstracción de una Computadora
+a) Muestra los componentes principales de la computadora
+b) Muestra el estado de la computadora (encendido o apagado)
+c) Crea una instancia y simula encender y apagar la computadora.
+"""
 
 
 from computadora import Computadora
